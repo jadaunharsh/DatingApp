e8ca69e94aa048592db87ebc1a8ff830b6e5ab44
jadaunharsh <jadaunharsh92@gmail.com>
2025-11-07 16:55:11 +0530

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,28 @@
+
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
