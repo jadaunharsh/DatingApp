ba091ae222d4c5df6554b27aa84e0729ef4be5d6
jadaunharsh <jadaunharsh92@gmail.com>
2025-12-27 22:51:19 +0530

    Error and exception handing with buggy controller.

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +45,7 @@ namespace API
 
             var app = builder.Build();
 
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseCors("CorsPolicy");
             app.UseAuthentication();
             app.UseAuthorization();
---
+++ client/src/shared/errors/server-error/server-error.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiError } from '../../../types/error';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-server-error',
+  imports: [],
+  templateUrl: './server-error.html',
+  styleUrl: './server-error.css',
+})
+export class ServerError {
+  protected error: any;
+  // protected error: ApiError;
+  private router = inject(Router);
+  private location = inject(Location);
+  protected showDetails = false;
+
+  constructor() {
+    const navigation = this.router.currentNavigation();
+    this.error = navigation?.extras?.state?.['error'];
+    console.log('test: ', this.error)
+  }
+
+
+  detailsToggle() {
+    this.showDetails = !this.showDetails;
+  }
+
+  goBack() {
+    this.location.back();
+  }
+}
