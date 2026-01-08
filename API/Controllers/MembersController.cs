ddeeebb276b26e869194859d93365b6b4241c90e
jadaunharsh <jadaunharsh92@gmail.com>
2026-01-09 00:48:15 +0530

    Template form, update member, ViewChild and caching implementation.

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,7 +1,10 @@
-﻿using API.Entities;
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -27,6 +30,26 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id) // localhost:5001/api/members/bob-id/photos
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.GetMemberId();
+
+            var member = await memberRepository.GetMemberForUpdate(memberId);
+            if(member == null) return BadRequest("Member not found");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            //memberRepository.Update(member); //optional
+
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update member");
         }
     }
 }
